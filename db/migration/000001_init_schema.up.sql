21b8b0a400bfe3118aae0bf5d3ead37c03eb6195
Antonino Adornetto <armaninobilling@gmail.com>
2023-04-15 14:02:24 -0400

    [EDIT] 1 account per email

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE `user` (
   `id` BINARY(16) PRIMARY KEY NOT NULL DEFAULT (UUID_TO_BIN(UUID())),
   `first_name` VARCHAR(20) NOT NULL,
   `last_name` VARCHAR(20) NOT NULL,
-  `email_address` TEXT NOT NULL,
+  `email_address` TEXT UNIQUE NOT NULL,
   `password` TEXT NOT NULL,
   `password_changed_at` DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00'
 );
