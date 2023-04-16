d134557cbe866987dd3ee4734fdb4267a008f4eb
Antonino Adornetto <armaninobilling@gmail.com>
2023-04-16 19:35:21 -0400

    [EDIT] rm pointless delete all record query

--- db/sqlc/muscle_group_test.go
+++ db/sqlc/muscle_group_test.go
@@ -12,16 +12,8 @@ func TestCreateMuscleGroup(t *testing.T) {
 	GenRandMuscleGroup(t)
 }
 
-func TestListMuscleGroup_EmptyList(t *testing.T) {
-	testQueries.DeleteAllMuscleGroups(context.Background())
-	muscleGroups, err := testQueries.ListMuscleGroups(context.Background())
-	require.NoError(t, err)
-	require.Empty(t, muscleGroups)
-}
-
 func TestListMuscleGroups(t *testing.T) {
 	n := 5
-	testQueries.DeleteAllMuscleGroups(context.Background())
 	muscleGroups := make([]MuscleGroup, n)
 
 	for i := 0; i < n; i++ {
@@ -30,12 +22,10 @@ func TestListMuscleGroups(t *testing.T) {
 
 	query, err := testQueries.ListMuscleGroups(context.Background())
 	require.NoError(t, err)
-	require.Len(t, query, n)
-	require.Equal(t, muscleGroups, query)
 
-	for i, v := range query {
-		require.Equal(t, v.ID, muscleGroups[i].ID)
-		require.Equal(t, v.Name, muscleGroups[i].Name)
+	for _, v := range query {
+		require.NotEmpty(t, v.ID)
+		require.NotEmpty(t, v.Name)
 	}
 }
 
