6cb5aec61ace7014ff39df0b576af4605272cc8a
Antonino Adornetto <armaninobilling@gmail.com>
2023-04-16 16:01:25 -0400

    [EDIT] update mg name unit test

--- db/sqlc/muscle_group_test.go
+++ db/sqlc/muscle_group_test.go
@@ -25,6 +25,25 @@ func TestListMuscleGroups(t *testing.T) {
 	require.GreaterOrEqual(t, len(query), n)
 }
 
+func TestUpdateMuscleGroupName(t *testing.T) {
+	muscleGroup := GenRandMuscleGroup(t)
+
+	newMuscleGroupName := util.RandomStr(5)
+
+	_, err := testQueries.UpdateMuscleGroup(context.Background(), UpdateMuscleGroupParams{
+		ID:   muscleGroup.ID,
+		Name: newMuscleGroupName,
+	})
+	require.NoError(t, err)
+
+	query, err := testQueries.GetMuscleGroup(context.Background(), muscleGroup.ID)
+	require.NoError(t, err)
+	require.NotZero(t, query.ID)
+	require.NotEqual(t, query.Name, muscleGroup.Name)
+	require.Equal(t, query.Name, newMuscleGroupName)
+	require.Equal(t, query.ID, muscleGroup.ID)
+}
+
 // asserts both create and get one queries
 func GenRandMuscleGroup(t *testing.T) MuscleGroup {
 	muscleGroupName := util.RandomStr(5)
