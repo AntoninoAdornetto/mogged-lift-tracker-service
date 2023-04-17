36fc5d3a69316cd8cdd0ca9d6736649f4776585e
Antonino Adornetto <armaninobilling@gmail.com>
2023-04-16 22:49:53 -0400

    [EDIT] unit tests for listing exercises

--- db/sqlc/exercise_test.go
+++ db/sqlc/exercise_test.go
@@ -11,21 +11,59 @@ import (
 
 // asserts both create and get one queries
 func TestCreateExercise(t *testing.T) {
-	GenRandExercise(t)
+	user := GenRandUser(t)
+	GenRandExercise(t, user.ID)
 }
 
-// @todo
-// func TestListExercises(t *testing.T) {
-// }
+func TestListExercises(t *testing.T) {
+	n := 5
+	user := GenRandUser(t)
+	otherUser := GenRandUser(t) // this user should not see the newly created exercises
+	exercises := make([]Exercise, n)
 
-func GenRandExercise(t *testing.T) Exercise {
+	for i := 0; i < n; i++ {
+		exercises[i] = GenRandExercise(t, user.ID)
+	}
+
+	query, err := testQueries.ListExercises(context.Background(), user.ID)
+	require.NoError(t, err)
+	require.Len(t, query, n)
+
+	creatorUserId, err := uuid.Parse(user.ID)
+	require.NoError(t, err)
+
+	otherUserId, err := uuid.Parse(otherUser.ID)
+	require.NoError(t, err)
+
+	for _, v := range query {
+		queryUserId, err := uuid.FromBytes(v.UserID)
+		require.NoError(t, err)
+		require.NotEmpty(t, v.ID)
+		require.NotEmpty(t, v.Name)
+		require.NotEmpty(t, v.Category)
+		require.NotEmpty(t, v.MuscleGroup)
+		require.NotEmpty(t, v.RestTimer)
+		require.False(t, v.Isstock)
+		require.Equal(t, queryUserId, creatorUserId)
+		require.Zero(t, v.MostRepsLifted)
+		require.Zero(t, v.MostWeightLifted)
+
+		// other users will not see the exercises
+		require.NotEqual(t, queryUserId, otherUserId)
+	}
+
+	otherUserQuery, err := testQueries.ListExercises(context.Background(), otherUser.ID)
+	require.NoError(t, err)
+	require.Empty(t, otherUserQuery)
+}
+
+func GenRandExercise(t *testing.T, userID string) Exercise {
 	exercise := &Exercise{}
 
 	exerciseName := util.RandomStr(10)
 	category := GenRandCategory(t)
 	muscleGroup := GenRandMuscleGroup(t)
-	user := GenRandUser(t)
-	userId, err := uuid.Parse(user.ID)
+	userId, err := uuid.Parse(userID)
 	require.NoError(t, err)
 
 	args := CreateExerciseParams{
