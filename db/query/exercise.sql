b525c60fc5c40f00e6d0ee51b3837541d70b72b9
Antonino Adornetto <armaninobilling@gmail.com>
2023-04-16 23:00:49 -0400

    [EDIT] add update query

--- db/query/exercise.sql
+++ db/query/exercise.sql
@@ -18,3 +18,13 @@ WHERE id = ? LIMIT 1;
 -- name: ListExercises :many
 SELECT * FROM exercise
 WHERE user_id = UUID_TO_BIN(sqlc.arg('user_id'));
+
+-- name: UpdateExercise :execresult
+UPDATE exercise SET
+name = IFNULL(sqlc.arg('name'), name),
+muscle_group = IFNULL(sqlc.arg('muscle_group'), muscle_group),
+category = IFNULL(sqlc.arg('category'), category),
+most_weight_lifted = IFNULL(sqlc.arg('most_weight_lifted'), most_weight_lifted),
+most_reps_lifted = IFNULL(sqlc.arg('most_reps_lifted'), most_reps_lifted),
+rest_timer = IFNULL(sqlc.arg('rest_timer'), rest_timer)
+WHERE user_id = UUID_TO_BIN(sqlc.arg('user_id'));
