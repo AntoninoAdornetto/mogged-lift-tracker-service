f5e0f9b834e537ef216e943c86cd60881e50bfc1
Antonino Adornetto <armaninobilling@gmail.com>
2023-04-17 22:48:32 -0400

    [ADD] CREATE/READ queries

---
+++ db/query/lift.sql
@@ -0,0 +1,18 @@
+-- name: CreateLift :execresult
+INSERT INTO lift (
+	exercise_name,
+	weight_lifted,
+	reps,
+	user_id,
+	workout_id
+) VALUES (
+	?,
+	?,
+	?,
+	UUID_TO_BIN(sqlc.arg('user_id')),
+	?
+);
+
+-- name: GetLift :one
+SELECT * FROM lift
+WHERE id = ? AND user_id = UUID_TO_BIN(sqlc.arg('user_id'));
