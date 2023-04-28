1142af783ce288a194099b936864e556e0ae8060
Antonino Adornetto <armaninobilling@gmail.com>
2023-04-28 00:30:22 -0400

    NewUserTx unit tests

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -10,6 +10,45 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestNewUserWithStockExercises(t *testing.T) {
+	store := NewStore(testDB)
+	user := GenRandUser(t)
+	userId, err := uuid.Parse(user.ID)
+	require.NoError(t, err)
+
+	// here we generated a new user the normal way, with out the transaction and stock exercises.
+	// this is to test that a user, when first created, will not have the stock exercises set in the user exercise table
+	exercises, err := store.ListExercises(context.Background(), userId.String())
+	require.NoError(t, err)
+	require.Empty(t, exercises)
+
+	args := CreateUserParams{
+		FirstName:    util.RandomStr(5),
+		LastName:     util.RandomStr(8),
+		Password:     util.RandomStr(10),
+		EmailAddress: util.RandomStr(5) + "@gmail.com",
+	}
+
+	// we do need to create a new user, but using the transaction now. This will set all stock exercises into the users exercise table
+	newUser, err := store.NewUserTx(context.Background(), args)
+	require.NoError(t, err)
+
+	stockExercises, err := store.ListStockExercies(context.Background())
+	require.NoError(t, err)
+	require.NotEmpty(t, stockExercises)
+
+	// at this point, we have not added any other exercises to this user. He will only have stock exercises
+	userExercises, err := store.ListExercises(context.Background(), newUser.ID.String())
+	require.NoError(t, err)
+	require.Len(t, stockExercises, len(userExercises))
+
+	for i, v := range stockExercises {
+		require.Equal(t, v.MuscleGroup, userExercises[i].MuscleGroup)
+		require.Equal(t, v.Name, userExercises[i].Name)
+		require.Equal(t, v.Category, userExercises[i].Category)
+	}
+}
+
 func TestWorkoutTx(t *testing.T) {
 	store := NewStore(testDB)
 	user := GenRandUser(t)
