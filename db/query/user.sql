0065fe71789a3e3df87060777b099910f9747481
Antonino Adornetto <armaninobilling@gmail.com>
2023-04-30 06:36:38 -0400

    [EDIT] needed to query for user by id

--- db/query/user.sql
+++ db/query/user.sql
@@ -11,6 +11,10 @@ INSERT INTO user (
 SELECT BIN_TO_UUID(id) as id, first_name, last_name, email_address, password_changed_at, password
 FROM user WHERE email_address = ? LIMIT 1;
 
+-- name: GetUserById :one
+SELECT BIN_TO_UUID(id) as id, first_name, last_name, email_address, password_changed_at, password
+FROM user WHERE id = UUID_TO_BIN('user_id') LIMIT 1;
+
 -- name: UpdateUser :exec
 UPDATE user SET
 first_name = IFNULL(sqlc.narg('first_name'), first_name),
