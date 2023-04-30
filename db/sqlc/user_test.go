7b07ea75b2800de8d3f1ca280f44012539da3a7a
Antonino Adornetto <armaninobilling@gmail.com>
2023-04-30 03:33:08 -0400

    [ADD] unit tests for changing password

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/AntoninoAdornetto/mogged-lift-tracker-service/util"
 	"github.com/stretchr/testify/require"
@@ -12,11 +13,23 @@ func TestCreateUser(t *testing.T) {
 	GenRandUser(t)
 }
 
+func TestGetUser(t *testing.T) {
+	user := GenRandUser(t)
+	query, err := testQueries.GetUser(context.Background(), user.EmailAddress)
+	require.NoError(t, err)
+	require.Equal(t, user.EmailAddress, query.EmailAddress)
+	require.Equal(t, user.Password, query.Password)
+	require.Equal(t, user.FirstName, query.FirstName)
+	require.Equal(t, user.LastName, query.LastName)
+	require.Equal(t, user.ID, query.ID)
+	require.Equal(t, user.PasswordChangedAt, query.PasswordChangedAt)
+}
+
 func TestUpdateUserFirstName(t *testing.T) {
 	user := GenRandUser(t)
 	newFirstName := util.RandomStr(5)
 
-	_, err := testQueries.UpdateUser(context.Background(), UpdateUserParams{
+	err := testQueries.UpdateUser(context.Background(), UpdateUserParams{
 		FirstName: newFirstName,
 		UserID:    user.ID,
 	})
@@ -35,7 +48,7 @@ func TestUpdateUserLastName(t *testing.T) {
 	user := GenRandUser(t)
 	newLastName := util.RandomStr(5)
 
-	_, err := testQueries.UpdateUser(context.Background(), UpdateUserParams{
+	err := testQueries.UpdateUser(context.Background(), UpdateUserParams{
 		LastName: newLastName,
 		UserID:   user.ID,
 	})
@@ -55,7 +68,7 @@ func TestUpdateEmail(t *testing.T) {
 	user := GenRandUser(t)
 	newEmailAddress := util.RandomStr(5) + "@gmail.com"
 
-	_, err := testQueries.UpdateUser(context.Background(), UpdateUserParams{
+	err := testQueries.UpdateUser(context.Background(), UpdateUserParams{
 		EmailAddress: newEmailAddress,
 		UserID:       user.ID,
 	})
@@ -71,17 +84,33 @@ func TestUpdateEmail(t *testing.T) {
 	require.Equal(t, updatedUser.EmailAddress, newEmailAddress)
 }
 
+func TestChangePassword(t *testing.T) {
+	user := GenRandUser(t)
+	require.Equal(t, user.PasswordChangedAt.Year(), 1970) // indicates password has never been changed
+
+	newPassword := util.RandomStr(12)
+	err := testQueries.ChangePassword(context.Background(), ChangePasswordParams{
+		Password: newPassword,
+		UserID:   user.ID,
+	})
+
+	updatedUser, err := testQueries.GetUser(context.Background(), user.EmailAddress)
+	require.NoError(t, err)
+	require.WithinDuration(t, updatedUser.PasswordChangedAt, time.Now(), time.Minute)
+	require.NoError(t, err)
+	require.NotEqual(t, user.Password, updatedUser.Password)
+}
+
 func TestDeleteUser(t *testing.T) {
 	user := GenRandUser(t)
 
-	_, err := testQueries.DeleteUser(context.Background(), user.ID)
+	err := testQueries.DeleteUser(context.Background(), user.ID)
 	require.NoError(t, err)
 
 	_, err = testQueries.GetUser(context.Background(), user.EmailAddress)
 	require.Error(t, err)
 }
 
-// tests both Create and Get
 func GenRandUser(t *testing.T) GetUserRow {
 	args := CreateUserParams{
 		FirstName:    util.RandomStr(10),
@@ -90,9 +119,8 @@ func GenRandUser(t *testing.T) GetUserRow {
 		EmailAddress: util.RandomStr(7) + "@gmail.com",
 	}
 
-	insert, err := testQueries.CreateUser(context.Background(), args)
+	err := testQueries.CreateUser(context.Background(), args)
 	require.NoError(t, err)
-	require.NotNil(t, insert)
 
 	user, err := testQueries.GetUser(context.Background(), args.EmailAddress)
 	require.NoError(t, err)
@@ -100,6 +128,8 @@ func GenRandUser(t *testing.T) GetUserRow {
 	require.Equal(t, args.FirstName, user.FirstName)
 	require.Equal(t, args.LastName, user.LastName)
 	require.Equal(t, args.EmailAddress, user.EmailAddress)
+	require.Equal(t, user.PasswordChangedAt.Year(), 1970) // indicates the password has never been changed
+	require.GreaterOrEqual(t, len(args.Password), 10)
 	require.NotNil(t, user.ID)
 
 	return user
