11d020d111642c7936f4850dc90098a6199d8dfe
Antonino Adornetto <armaninobilling@gmail.com>
2023-04-30 03:32:56 -0400

    [SQLC] generated

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -52,7 +52,7 @@ func (store *Store) NewUserTx(ctx context.Context, args CreateUserParams) (NewUs
 	err := store.execTx(ctx, func(q *Queries) error {
 		var err error
 
-		_, err = store.CreateUser(ctx, CreateUserParams{
+		err = store.CreateUser(ctx, CreateUserParams{
 			LastName:     args.LastName,
 			Password:     args.Password,
 			FirstName:    args.FirstName,
