b35510f88c8b22995b40bbf825d24167d2561b12
Antonino Adornetto <armaninobilling@gmail.com>
2023-04-30 07:49:30 -0400

    [FIX] update stmt was broken

--- db/query/user.sql
+++ db/query/user.sql
@@ -7,7 +7,7 @@ INSERT INTO user (
 	password
 ) VALUES (UUID_TO_BIN(UUID()),?, ?, ?, ?);
 
--- name: GetUser :one
+-- name: GetUserByEmail :one
 SELECT BIN_TO_UUID(id) as id, first_name, last_name, email_address, password_changed_at, password
 FROM user WHERE email_address = ? LIMIT 1;
 
@@ -17,10 +17,11 @@ FROM user WHERE id = UUID_TO_BIN(sqlc.arg('user_id')) LIMIT 1;
 
 -- name: UpdateUser :exec
 UPDATE user SET
-first_name = IFNULL(sqlc.narg('first_name'), first_name),
-last_name = IFNULL(sqlc.narg('last_name'), last_name),
-email_address = IFNULL(sqlc.narg('email_address'), email_address)
+	first_name = COALESCE(sqlc.narg('first_name'), first_name),
+	last_name = COALESCE(sqlc.narg('last_name'), last_name),
+	email_address = COALESCE(sqlc.narg('email_address'), email_address)
 WHERE id = UUID_TO_BIN(sqlc.arg('user_id'));
+
 
 -- name: ChangePassword :exec
 UPDATE user SET
