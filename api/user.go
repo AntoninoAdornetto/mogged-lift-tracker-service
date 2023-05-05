5a3d65e78c21e73fd284b9a80ad49f6cb670f8ad
Antonino Adornetto <armaninobilling@gmail.com>
2023-05-05 18:04:08 -0400

    [EDIT] so user can see what has changed

--- api/user.go
+++ api/user.go
@@ -159,7 +159,17 @@ func (server *Server) updateUser(ctx *gin.Context) {
 		return
 	}
 
-	ctx.JSON(http.StatusNoContent, nil)
+	user, err := server.store.GetUserById(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(fmt.Errorf(USERID_NOT_FOUND, req.ID)))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, user)
 }
 
 type updatePasswordRequest struct {
