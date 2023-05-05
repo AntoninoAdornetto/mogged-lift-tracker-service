ce00876bedc2d5ab846d5c7248a545cd7b6dec5b
Antonino Adornetto <armaninobilling@gmail.com>
2023-05-05 00:24:04 -0400

    [ADD] allow user to change password

--- api/user.go
+++ api/user.go
@@ -158,7 +158,49 @@ func (server *Server) updateUser(ctx *gin.Context) {
 		return
 	}
 
-	ctx.JSON(http.StatusOK, nil)
+	ctx.JSON(http.StatusNoContent, nil)
+}
+
+type updatePasswordRequest struct {
+	ID              string `json:"id" binding:"required"`
+	CurrentPassword string `json:"currentPassword" binding:"required,gt=8"`
+	NewPassword     string `json:"newPassword" binding:"required,gt=8"`
+}
+
+func (server *Server) changePassword(ctx *gin.Context) {
+	req := &updatePasswordRequest{}
+	if err := ctx.ShouldBindJSON(req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	user, err := server.store.GetUserById(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(fmt.Errorf(USERID_NOT_FOUND, req.ID)))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	if user.Password != req.CurrentPassword {
+		ctx.JSON(http.StatusUnauthorized, errorResponse(errors.New("your current password is incorrect")))
+		return
+	}
+
+	args := db.ChangePasswordParams{
+		Password: req.NewPassword,
+		UserID:   req.ID,
+	}
+
+	err = server.store.ChangePassword(ctx, args)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusNoContent, nil)
 }
 
 type deleteUserRequest struct {
