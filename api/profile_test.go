6af2f70ba403ac175cc12e072beaae9676abdc5c
Antonino Adornetto <armaninobilling@gmail.com>
2023-05-06 02:22:30 -0400

    [ADD] unit tests -> todo

---
+++ api/profile_test.go
@@ -0,0 +1,47 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	mockdb "github.com/AntoninoAdornetto/mogged-lift-tracker-service/db/mock"
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateProfile(t *testing.T) {
+	testCases := []struct {
+		Name       string
+		Body       gin.H
+		buildStubs func(store *mockdb.MockStore)
+		checkRes   func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{}
+
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.Name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+
+			payload, err := json.Marshal(tc.Body)
+			require.NoError(t, err)
+
+			url := "/createUser"
+			request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(payload))
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+			tc.checkRes(t, recorder)
+		})
+	}
+}
