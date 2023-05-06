b069dd45a78529ca6a555c148a3ef97b64291d65
Antonino Adornetto <armaninobilling@gmail.com>
2023-05-06 02:08:23 -0400

    [EDIT] add handler for update profile

--- api/profile.go
+++ api/profile.go
@@ -100,22 +100,73 @@ func (server *Server) getProfile(ctx *gin.Context) {
 	})
 }
 
-// type updateProfileRequest struct {
-// 	Country           string  `json:"country" binding:"required"`
-// 	MeasurementSystem string  `json:"measurementSystem" binding:"required"`
-// 	BodyWeight        float64 `json:"bodyWeight" binding:"required"`
-// 	BodyFat           float64 `json:"bodyFat" binding:"required"`
-// 	TimeZoneOffset    int32   `json:"timeZoneOffset" binding:"required"`
-// }
+type updateProfileRequest struct {
+	Country           string  `json:"country"`
+	MeasurementSystem string  `json:"measurementSystem"`
+	BodyWeight        float64 `json:"bodyWeight"`
+	BodyFat           float64 `json:"bodyFat"`
+	TimeZoneOffset    int32   `json:"timeZoneOffset"`
+	UserID            string  `json:"userID" binding:"required,gt=-721,lt=821"`
+}
 
-// func (server *Server) updateProfile(ctx *gin.Context) {
-// 	req := updateProfileRequest{}
-// 	if err := ctx.ShouldBindJSON(&req); err != nil {
-// 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-// 		return
-// 	}
+func (server *Server) updateProfile(ctx *gin.Context) {
+	req := updateProfileRequest{}
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
 
-// 	var country, measurementSystem sql.NullString
-// 	var bodyWeight, bodyFat sql.NullFloat64
-// 	var timeZoneOffset sql.NullInt32
-// }
+	_, err := server.store.GetProfile(ctx, req.UserID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(fmt.Errorf(PROFILE_NOT_FOUND, req.UserID)))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	args := db.UpdateProfileParams{
+		Country: sql.NullString{
+			Valid:  req.Country != "",
+			String: req.Country,
+		},
+		MeasurementSystem: sql.NullString{
+			Valid:  req.MeasurementSystem != "",
+			String: req.MeasurementSystem,
+		},
+		BodyFat: sql.NullFloat64{
+			Valid:   req.BodyFat > 0,
+			Float64: req.BodyFat,
+		},
+		BodyWeight: sql.NullFloat64{
+			Valid:   req.BodyWeight > 0,
+			Float64: req.BodyWeight,
+		},
+		TimezoneOffset: sql.NullInt32{
+			Valid: req.TimeZoneOffset >= -720 && req.TimeZoneOffset <= 840,
+			Int32: req.TimeZoneOffset,
+		},
+		UserID: req.UserID,
+	}
+
+	_, err = server.store.UpdateProfile(ctx, args)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	profile, err := server.store.GetProfile(ctx, req.UserID)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, ProfileResponse{
+		Country:           profile.Country,
+		MeasurementSystem: profile.MeasurementSystem,
+		BodyWeight:        profile.BodyWeight,
+		BodyFat:           profile.BodyFat,
+		TimeZoneOffset:    profile.TimezoneOffset,
+	})
+}
