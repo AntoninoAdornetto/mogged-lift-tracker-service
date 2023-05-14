c3239c9b23a585d6a1bbc81578a72c03ac621076
Antonino Adornetto <armaninobilling@gmail.com>
2023-05-14 02:05:40 -0400

    [ADD] unit tests

---
+++ token/jwt_test.go
@@ -0,0 +1,66 @@
+package token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/AntoninoAdornetto/mogged-lift-tracker-service/util"
+	"github.com/golang-jwt/jwt"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestJWTMaker(t *testing.T) {
+	maker, err := NewJWTMaker(util.RandomStr(32))
+	require.NoError(t, err)
+
+	userID := uuid.New().String()
+	duration := time.Minute
+
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, err := maker.CreateToken(userID, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, userID, payload.UserID)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
+}
+
+func TestExpiredToken(t *testing.T) {
+	maker, err := NewJWTMaker(util.RandomStr(32))
+	require.NoError(t, err)
+
+	token, err := maker.CreateToken(uuid.New().String(), -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ExpiredTokenError.Error())
+	require.Nil(t, payload)
+}
+
+func TestInvalidTokenAlgNone(t *testing.T) {
+	payload, err := NewPayload(uuid.New().String(), time.Minute)
+	require.NoError(t, err)
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodNone, payload)
+	token, err := jwtToken.SignedString(jwt.UnsafeAllowNoneSignatureType)
+	require.NoError(t, err)
+
+	maker, err := NewJWTMaker(uuid.New().String())
+	require.NoError(t, err)
+
+	payload, err = maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, InvalidTokenError.Error())
+	require.Nil(t, payload)
+}
