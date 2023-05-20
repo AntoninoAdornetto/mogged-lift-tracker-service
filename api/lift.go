226c327c8f58bfc65f5659a9a9d4f53f79bf8db4
Antonino Adornetto <armaninobilling@gmail.com>
2023-05-19 22:21:22 -0400

    [ADD] LIST lifts from wo handler

--- api/lift.go
+++ api/lift.go
@@ -57,3 +57,41 @@ func (server *Server) getLift(ctx *gin.Context) {
 		WorkoutID:    lift.WorkoutID,
 	})
 }
+
+type listLiftsFromWorkoutRequest struct {
+	WorkoutID int32 `uri:"workout_id"`
+}
+
+func (server *Server) listLiftsFromWorkout(ctx *gin.Context) {
+	userID := ctx.MustGet(authorizationPayloadKey).(*token.Payload).UserID
+
+	req := listLiftsFromWorkoutRequest{}
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	lifts, err := server.store.ListLiftsFromWorkout(ctx, db.ListLiftsFromWorkoutParams{
+		WorkoutID: req.WorkoutID,
+		UserID:    userID,
+	})
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	res := make([]LiftResponse, len(lifts))
+	for i, v := range lifts {
+		res[i] = LiftResponse{
+			ID:           v.ID,
+			Reps:         v.Reps,
+			WeightLifted: v.WeightLifted,
+			ExerciseName: v.ExerciseName,
+			SetType:      v.SetType,
+			UserID:       userID,
+			WorkoutID:    v.WorkoutID,
+		}
+	}
+
+	ctx.JSON(http.StatusOK, res)
+}
