833a4478d588238b6affab837cf2656e3cb08b65
Antonino Adornetto <armaninobilling@gmail.com>
2023-05-21 14:18:17 -0400

    [EDIT] follow convention of other query -> nargs

--- db/query/template.sql
+++ db/query/template.sql
@@ -19,9 +19,9 @@ WHERE created_by = UUID_TO_BIN(sqlc.arg('created_by'));
 
 -- name: UpdateTemplate :exec
 UPDATE template SET
-	name = COALESCE(sqlc.arg('name'), name),
+	name = COALESCE(sqlc.narg('name'), name),
 	exercises = COALESCE(sqlc.arg('exercises'), exercises),
-	date_last_used = COALESCE(sqlc.arg('date_last_used'), date_last_used)
+	date_last_used = COALESCE(sqlc.narg('date_last_used'), date_last_used)
 WHERE id = ? AND created_by = UUID_TO_BIN(sqlc.arg('created_by'));
 
 -- name: DeleteTemplate :exec
