f78666cb7713565c7f145364b35847f68dc91164
Antonino Adornetto <armaninobilling@gmail.com>
2023-06-01 22:57:50 -0400

    [EDIT] easier file read in db tests

--- db/trigger/inactive.sql
+++ db/trigger/inactive.sql
@@ -1,3 +1,1 @@
-CREATE TRIGGER `inactive` AFTER DELETE ON user
-	FOR EACH ROW
-		INSERT INTO `inactive_user` (id) VALUES (OLD.id);
+CREATE TRIGGER inactive AFTER DELETE ON user FOR EACH ROW INSERT INTO inactive_user (id) VALUES (OLD.id);
