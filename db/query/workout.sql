20f170ad2f640be94fd1fab20ef563c2528b69db
Antonino Adornetto <armaninobilling@gmail.com>
2023-06-29 21:34:13 -0400

    [EDIT] wo completion & dashboard queries

--- db/query/workout.sql
+++ db/query/workout.sql
@@ -20,9 +20,20 @@ WHERE user_id = UUID_TO_BIN(sqlc.arg('user_id'));
 -- name: UpdateWorkout :exec
 UPDATE workout SET
 	duration = COALESCE(sqlc.narg('duration'), duration),
-	lifts = COALESCE(sqlc.narg('lifts'), lifts)
+	lifts = COALESCE(sqlc.narg('lifts'), lifts),
+	completed_date = COALESCE(sqlc.narg('completed_date'), completed_date)
 WHERE id = ? AND user_id = UUID_TO_BIN(sqlc.arg('user_id'));
 
 -- name: DeleteWorkout :exec
 DELETE FROM workout 
 WHERE id = ? AND user_id = UUID_TO_BIN(sqlc.arg('user_id'));
+
+-- name: GetTotalWorkouts :one
+SELECT COUNT(*) FROM workout
+WHERE user_id = UUID_TO_BIN(sqlc.arg('user_id'));
+
+-- name: GetLastWorkout :one
+SELECT * FROM workout
+WHERE user_id = UUID_TO_BIN(sqlc.arg('user_id'))
+ORDER BY completed_date DESC
+LIMIT 1;
